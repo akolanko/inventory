dd1dda582f8cb1bf7c67e7402363718ccada954d
Aleksandra Kolanko <atkolanko@gmail.com>
2014-05-05 21:45:12 -0400

    move storage, credentials, etc under one has_attached_file

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,12 +12,10 @@ class Product < ActiveRecord::Base
 	validates :price, numericality: {only_integer: true}, allow_nil: true
 	validates :inventory, presence: true, numericality: {only_integer: true}
 
-	has_attached_file :image, :styles => {:medium => "400x400"}, :default_url => "/images/:style/missing.jpg"
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg"]
-
-	has_attached_file :download, :storage => :s3, :s_3credentials => Proc.new{|a| a.instance.s3_credentials }
+	has_attached_file :image, :styles => {:medium => "400x400"}, :default_url => "/images/:style/missing.jpg", :storage => :s3, :s_3credentials => Proc.new{|a| a.instance.s3_credentials }
 	def credentails
 		{:bucket => '[AWS_BUCKET]', :access_key_id => ENV['AWS_KEY'], :secret_access_key => ENV['AWS_SECRET']}
 	end
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg"]
 
 end
