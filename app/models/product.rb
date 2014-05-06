f5bf1b95856351c6e8934b283a90e93433f6b460
Aleksandra Kolanko <atkolanko@gmail.com>
2014-05-05 23:18:28 -0400

    back to no aws

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,9 +12,6 @@ class Product < ActiveRecord::Base
 	validates :price, numericality: {only_integer: true}, allow_nil: true
 	validates :inventory, presence: true, numericality: {only_integer: true}
 
-	has_attached_file :image, :styles => {:medium => "400x400"}, :default_url => "/images/:style/missing.jpg", :storage => :s3, :s_3credentials => Proc.new{|a| a.instance.s3_credentials }
-	def credentails
-		{:bucket => '[AWS_BUCKET]', :access_key_id => ENV['AWS_KEY'], :secret_access_key => ENV['AWS_SECRET']}
-	end
+	has_attached_file :image, :styles => {:medium => "400x400"}, :default_url => "/images/:style/missing.jpg"
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg"]
 end
